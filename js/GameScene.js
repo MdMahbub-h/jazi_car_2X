a1d4f7962b51dd028e4e6b5579f827f6c90136fa
MdMahbub-h <2021339001@student.sust.edu>
2025-11-05 15:58:06 +0600

    Update GameScene.js

--- js/GameScene.js
+++ js/GameScene.js
@@ -13,7 +13,7 @@ class GameScene extends Phaser.Scene {
     this.obstacleSpawnRate = 2;
     this.coinSpawnRate = 2.5;
     this.jaziItemSpawnRate = 0.4;
-    this.gameDuration = 6;
+    this.gameDuration = 60;
     this.coinPoints = 5;
     this.jaziItemPoints = 25;
     this.obstaclePointsPenalty = -5;
@@ -612,3 +612,4 @@ class GameScene extends Phaser.Scene {
 }
 
 export default GameScene;
+
